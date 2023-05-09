c14ab51469eee7c2bd3aa7c66e30ede088f1fd6f
Bagus Merta <merta.bagus24@gmail.com>
2023-05-09 16:15:09 +0800

    add 1 medium prob

--- kotlin/medium-leetcode/circularQueue.kt
+++ kotlin/medium-leetcode/circularQueue.kt
@@ -88,14 +88,3 @@ class MyCircularQueue(k: Int) {
     }
 
 }
-
-/**
- * Your MyCircularQueue object will be instantiated and called as such:
- * var obj = MyCircularQueue(k)
- * var param_1 = obj.enQueue(value)
- * var param_2 = obj.deQueue()
- * var param_3 = obj.Front()
- * var param_4 = obj.Rear()
- * var param_5 = obj.isEmpty()
- * var param_6 = obj.isFull()
- */
